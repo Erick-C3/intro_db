e2915460a8cdb02e68e7245e98c72508334a3ec0
erick <>
2024-06-06 12:02:58 -0300

    agregados comentarios

--- comandos.sql
+++ comandos.sql
@@ -1,12 +1,12 @@
 /* mysql.exe -u root -p  */
 
-SHOW DATABASES;
+SHOW DATABASES; /* para ver todas las base de datos actuales */
 
-DROP DATABASE nombre_db;
+DROP DATABASE nombre_db; /* para eliminar una base de datos */
 
-USE nombre_db; 
+USE nombre_db; /* para usar una base de datos */
 
-
+/* para crear una tabla en una base de datos */
 CREATE TABLE empleado (
     id INT AUTO_INCREMENT,
     nombre VARCHAR(50) NOT NULL,
@@ -14,13 +14,17 @@ CREATE TABLE empleado (
 );
 
 
+/* para mostrar todas las tablas en una base de datos */
 SHOW TABLES;
 
-
+/* describe una tabla */
 DESCRIBE empleado;
 
+
+/* muestra todo el contenido de los registros en una tabla */
 SELECT * FROM empleado;
 
+/* inserta un nuevo registro en una tabla */
 INSERT INTO empleado (nombre) 
     VALUES ("Erick");
 
